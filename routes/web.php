1582242f16f12a8bdc36f6c9132bf8a8f40c2cfa
Archi-R <kkromans009@gmail.com>
2020-05-30 09:01:21 -0400

    manejando rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return 'welcome route';
+});
+
+Route::get('/welcome/khris', function () {
+    return 'welcome route khris';
+});
+
+Route::get('/welcome/khris/pineda', function () {
+    return 'welcome route khris pineda';
+});
