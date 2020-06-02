4f12383c3afb80b48f60290f8626b45a5d7db160
khr1stopher <kkromans009@gmail.com>
2020-06-02 00:29:22 -0400

    learn laravel eloquent

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 class PagesController extends Controller
 {
     public function inicio(){
-    	return view('welcome');
+        $notas = App\Nota::all();
+    	return view('welcome', compact('notas'));
     }
 
     public function welcome()
